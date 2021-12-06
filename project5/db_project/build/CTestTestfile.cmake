aad947c785127c369775766c7469f9362579e421
2018007429 <2018007429@hanyang.ac.kr>
2021-12-05 22:07:20 -0800

    commit

---
+++ project5/db_project/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/cantsleep/2021_ite2038_2018007429/project5/db_project
+# Build directory: /home/cantsleep/2021_ite2038_2018007429/project5/db_project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("db")
+subdirs("test")
---
+++ project5/db_project/build/_deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/cantsleep/2021_ite2038_2018007429/project5/db_project/build/_deps/googletest-src
+# Build directory: /home/cantsleep/2021_ite2038_2018007429/project5/db_project/build/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
