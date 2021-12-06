65586d7fb9080db45132aafa9ba160fffb8e852f
2018007429 <2018007429@hanyang.ac.kr>
2021-12-06 20:37:33 +0900

    commit build

--- project5/db_project/db/src/trx.cc
+++ project5/db_project/db/src/trx.cc
@@ -10,14 +10,14 @@ int trx_begin(){
 
     trx_t *trx = (trx_t*)malloc(sizeof(trx_t));
     
-    // if(trx_id <= 0 ){
-    //     trx_id = 1;
-    // }
+    if(trx_id <= 0 ){
+        trx_id = 1;
+    }
     
-    // trx->trx_id = trx_id;
-    // trx->next_lock =NULL;
-    // trx->next_trx = NULL;
-    // HASH_ADD(hh, trx_table, trx_id, sizeof(int), trx);
+    trx->trx_id = trx_id;
+    trx->next_lock =NULL;
+    trx->next_trx = NULL;
+    HASH_ADD(hh, trx_table, trx_id, sizeof(int), trx);
 
     pthread_mutex_unlock(&trx_table_latch);
 
@@ -31,12 +31,12 @@ int trx_commit(int trx_id){
 
     lock_t* lock;
     trx_t * trx;
-    // trx->trx_id = trx_id;
+    trx->trx_id = trx_id;
     
     HASH_FIND(hh, trx_table, &trx->trx_id, sizeof(int), trx);
-    // lock = trx->next_lock;
-    // lock_t* tmp_lock = trx->next_lock;
-    // lock_release(lock);
+    lock = trx->next_lock;
+    lock_t* tmp_lock = trx->next_lock;
+    lock_release(lock);
 
     // while(tmp_lock != NULL){
     //     tmp_lock = tmp_lock->trx_next_lock;
